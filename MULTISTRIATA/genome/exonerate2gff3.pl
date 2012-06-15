dd3f12c61c71488982ea3ea32a831a5d5f68cd61
remo <remo@Z800.szn>
2012-06-15 19:19:42 +0200

    exonerate parser update

--- MULTISTRIATA/genome/exonerate2gff3.pl
+++ MULTISTRIATA/genome/exonerate2gff3.pl
@@ -4,37 +4,40 @@ use warnings;
 use Data::Dumper;
 
 my $filename = $ARGV[0];
+my $debug = $ARGV[1];
+die "\n\tUSAGE: perl $0 [exonerate output] [debug]\n\n" unless $ARGV[0];
 die "\n\tERROR: Cannot find the file $ARGV[0]\n\n" unless -e $ARGV[0];
 
 open(IN,$filename);
 
-my $res;
-my $block;
 my $row;
+my $block;
+my $ingff;
+my $res;
 
 while($row = <IN>) {
   if($row =~ /START OF GFF DUMP/) {
     $block = 1;
+    $ingff = 1;
     while($block) {
       $row = <IN>;
+      if($row =~ /END OF GFF DUMP/) {
+        undef $ingff;
+      }
       next if $row =~ /^\#/;
       if($row =~ /C4 Alignment/ || $row =~ /completed exonerate analysis/) {
         convert($res);
-        undef $res;
+        print Dumper $res if $debug;
         undef $block;
+        undef $ingff;
+        undef $res;
       }
-      else {
-        if($row =~ /^\>/) {
-          $res->{custom} .= $row;
-        }
-        else {
-          $res->{gff2} .= $row;
-        }
-      }
+      $res->{gff2} .= $row if $ingff && $block;
+      $res->{custom} .= $row if !$ingff && $block;
     }
   }
 }
-  
+
 sub convert {
   my $res = shift;
   my $gff = $res->{gff2};
