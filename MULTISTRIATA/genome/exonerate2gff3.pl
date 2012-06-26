ad96f737825e6e096daade9095b03c6f33234da4
remo <remo@Z800.szn>
2012-06-26 16:27:48 +0200

    Added number of occurrences at the end of the id in exonerate2gff3.pl to account for multiple mapped transcripts

--- MULTISTRIATA/genome/exonerate2gff3.pl
+++ MULTISTRIATA/genome/exonerate2gff3.pl
@@ -14,6 +14,7 @@ my $row;
 my $block;
 my $ingff;
 my $res;
+my $seen;
 
 while($row = <IN>) {
   if($row =~ /START OF GFF DUMP/) {
@@ -47,7 +48,9 @@ sub convert {
     my @field = split(/\t/,$row);
     if($field[2] eq 'gene') {
       $field[8] =~ /sequence (.+) ; gene_orientation/;
-      $id = $1;
+      my $preid = $1;
+      $seen->{$preid} ++;
+      $id = $preid.'.'.$seen->{$preid};
       $string = "$field[0]\texonerate\tmRNA\t$field[3]\t$field[4]\t$field[5]\t$field[6]\t$field[7]\tID=$id\n";
     }
     elsif($field[2] eq 'exon') {
